a5bad93c6533a78d011deecd1a5f0f2129beb153
nmammadova16 <narmin.mammadova16@gmail.com>
2023-03-30 14:09:17 +0400

    FirstTest

---
+++ sum.test.js
@@ -0,0 +1,23 @@
+const sum = require("./sum.js");
+
+describe("sum suite", function() {
+  test("Should add 2 positive numbers together and return the result", function() {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test("Should add 2 negative numbers together and return the result", function() {
+    expect(sum(-1, -2)).toBe(-3);
+  });
+
+  test("Should add 1 positive and 1 negative numbers together and return the result", function() {
+    expect(sum(-1, 2)).toBe(1);
+  });
+
+  test("Should add 1 positive and 0 together and return the result", function() {
+    expect(sum(0, 2)).toBe(2);
+  });
+
+  test("Should add 1 negative and 0 together and return the result", function() {
+    expect(sum(0, -2)).toBe(-2);
+  });
+});
